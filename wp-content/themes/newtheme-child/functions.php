34c6da0d7c2232549e7b09145e47c803cf5a1108
Noel Hoarau <n.hoarau75@gmail.com>
2020-03-17 19:36:49 +0100

    first commit

---
+++ wp-content/themes/newtheme-child/functions.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+			function theme_enqueue_styles() {
+			wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+			}
+
+			
+			// Le code ci-dessus permet de récupérer le fichier css du parent.
+
+add_action('init', 'themeperso_add_menu');
+	function themeperso_add_menu(){register_nav_menu('footer_menu', 'Menu pied de page');}
